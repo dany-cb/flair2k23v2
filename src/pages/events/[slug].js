88212b2a89e8901963bf4e9146f36d98235fb04b
Allen Chrisso Dhas <allencdvja@gmail.com>
2023-08-28 12:18:19 +0530

    feat: done - 2
    Co-authored-by: alwinjebastine <alwinjebastine@gmail.com>
    Co-authored-by: aravind2813 <aravindben576@gmail.com>

--- src/pages/events/[slug].js
+++ src/pages/events/[slug].js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Header from "../../components/Header";
+import Header from "../../components/header";
 import { useRouter } from "next/router";
 import styles from "../../styles/hero.module.css";
 
